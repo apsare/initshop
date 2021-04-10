6478796c15bd943e1227ec7ed5692f14cf3c35f4
matthieu LABBAYE <matthieu.labbaye@free.fr>
2021-04-10 17:03:11 +0200

    init ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { ProductsComponent } from './components/shop/products/products.component';
+import { SingleProductComponent } from './components/shop/single-product/single-product.component';
+import { CartComponent } from './components/shop/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProductsComponent,
+    SingleProductComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule
