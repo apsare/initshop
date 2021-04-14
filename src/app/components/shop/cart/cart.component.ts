db69e793d1570427d3060fa6cf6081e4b430acb3
mlabbayeasape <m.labbaye@asape.fr>
2021-04-14 17:47:22 +0200

    chapitre 71 encours

--- src/app/components/shop/cart/cart.component.ts
+++ src/app/components/shop/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/models/cart';
+import { CartService } from 'src/app/services/cart.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  cart: Cart[]= [];
+  urlImage = `${environment.urlImage}`;
+
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.cart = this.cartService.cart;
   }
 
 }
