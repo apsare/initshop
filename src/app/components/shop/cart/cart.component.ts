3c4ae3850626df18ea36dceafa912c3b6696fa8d
mlabbayeasape <m.labbaye@asape.fr>
2021-04-15 09:44:02 +0200

    video 71 terminée

--- src/app/components/shop/cart/cart.component.ts
+++ src/app/components/shop/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cart } from 'src/app/models/cart';
+import { Products } from 'src/app/models/products';
 import { CartService } from 'src/app/services/cart.service';
 import { environment } from 'src/environments/environment';
 
@@ -17,6 +18,14 @@ export class CartComponent implements OnInit {
 
   ngOnInit(): void {
     this.cart = this.cartService.cart;
+  }
+
+  addProduct(product: Products): void{
+    this.cartService.addProductToCart(product);
+  }
+
+  deleteProduct(product: Products): void{
+    this.cartService.deleteFromCart(product);
   }
 
 }
